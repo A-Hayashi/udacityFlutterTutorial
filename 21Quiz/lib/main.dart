87073b9d279012d764d6da36bb90f1292a7e8894
akionori <a.hayashi0113@gmail.com>
2021-10-04 22:31:47 +0900

    2

--- 21Quiz/lib/main.dart
+++ 21Quiz/lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'category_route.dart';
 
-void main() {
-  runApp(const UnitConverterApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
 }
 
-class UnitConverterApp extends StatelessWidget {
-  const UnitConverterApp({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
+  @override
+  createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      home: CategoryRoute(),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter++;
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
